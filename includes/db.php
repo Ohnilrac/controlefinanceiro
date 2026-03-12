4191ca48abecfaeb4386b4347ef7bbab99705f46
Jonas Nunes <jonasctps@gmail.com>
2026-03-12 09:16:25 -0300

    feat: configure FTP and update index.php

--- includes/db.php
+++ includes/db.php
@@ -4,6 +4,7 @@ require_once 'config.php';
 
 try {
     $pdo = new PDO('mysql:dbname='.DB_NAME.';host='.DB_HOST, DB_USER, DB_PASS . ';charset=utf8');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die( "Erro ao conectar com o banco de dados: " . $e->getMessage());
 }
